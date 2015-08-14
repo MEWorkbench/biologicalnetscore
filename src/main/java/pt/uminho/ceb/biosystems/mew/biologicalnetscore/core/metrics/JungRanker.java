dbd64c1644bcc36cf4f3b317720842c7a410b445
hugogiesteira <hugommg18@gmail.com>
2015-08-14 18:46:13 +0100

    [update] added JungNode and JungEdge

--- src/main/java/pt/uminho/ceb/biosystems/mew/biologicalnetscore/core/metrics/JungRanker.java
+++ src/main/java/pt/uminho/ceb/biosystems/mew/biologicalnetscore/core/metrics/JungRanker.java
@@ -2,34 +2,35 @@ package pt.uminho.ceb.biosystems.mew.biologicalnetscore.core.metrics;
 
 import java.io.Serializable;
 
-import pt.uminho.ceb.biosystems.mew.biologicalnetscore.core.network.IEdge;
-import pt.uminho.ceb.biosystems.mew.biologicalnetscore.core.network.INode;
-import pt.uminho.ceb.biosystems.mew.biologicalnetscore.core.network.metrics.IRanker;
 import edu.uci.ics.jung.algorithms.importance.AbstractRanker;
 import edu.uci.ics.jung.algorithms.scoring.VertexScorer;
+import pt.uminho.ceb.biosystems.mew.biologicalnetscore.core.network.INode;
+import pt.uminho.ceb.biosystems.mew.biologicalnetscore.core.network.JungEdge;
+import pt.uminho.ceb.biosystems.mew.biologicalnetscore.core.network.JungNode;
+import pt.uminho.ceb.biosystems.mew.biologicalnetscore.core.network.metrics.IRanker;
 
 
 public class JungRanker implements Serializable, IRanker
 {
     private static final long serialVersionUID = 1L;
-    protected AbstractRanker<INode, IEdge> ranker;
-    protected VertexScorer<INode, Double> vertexRanker;
+    protected AbstractRanker<JungNode, JungEdge> ranker;
+    protected VertexScorer<JungNode, Double> vertexRanker;
     
-    public JungRanker(AbstractRanker<INode, IEdge> ranker) {
+    public JungRanker(AbstractRanker<JungNode, JungEdge> ranker) {
         this.ranker = ranker;
         this.vertexRanker = null;
     }
     
-    public JungRanker(VertexScorer<INode, Double> vertexRanker) {
+    public JungRanker(VertexScorer<JungNode, Double> vertexRanker) {
         this.ranker = null;
         this.vertexRanker = vertexRanker;
     }
     
     public double getRankScore(INode n) {
         if (this.ranker != null) {
-            return this.ranker.getVertexRankScore(n);
+            return this.ranker.getVertexRankScore((JungNode)n);
         }
-        return (double)this.vertexRanker.getVertexScore(n);
+        return (double)this.vertexRanker.getVertexScore((JungNode)n);
     }
 
 }
